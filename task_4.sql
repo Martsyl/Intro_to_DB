cf8eea31778ac8345751faa26c3248e29d881781
martsyl <msnonuorah3964@gmail.com>
2025-12-14 23:58:08 +0100

    trying to show table

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,1 @@
--- Get full description of books table without using DESCRIBE or EXPLAIN
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+SHOW CREATE TABLE books;
