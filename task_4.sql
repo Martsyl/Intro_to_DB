edf4de38001028f538226dd7052062909703f175
martsyl <msnonuorah3964@gmail.com>
2025-12-14 23:48:56 +0100

    fixing erors

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,15 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+-- Get full description of books table without using DESCRIBE or EXPLAIN
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
