bc66c657d5b0ee4b5855782ca684d9bbafe2bbd6
martsyl <msnonuorah3964@gmail.com>
2025-12-14 23:55:23 +0100

    task four correction

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,15 @@
 -- Get full description of books table without using DESCRIBE or EXPLAIN
 SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'Books'
+    AND TABLE_NAME = 'books'
 ORDER BY 
     ORDINAL_POSITION;
